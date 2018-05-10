5fddf598ff4eb6a9e9d1cdced8503b961609af71
Nikita Zhelezkov <nikita.e.zhelezkov@gmail.com>
2018-05-11 01:03:09 +0300

    fixed max x

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import max from "lodash/max";
 import { createBasisPolynoms, interpolateByLagrange } from "./calc";
 import { toFn } from "./utils";
 import Chart from "./Chart";
 
-import 'react-vis/dist/style.css'
+import "react-vis/dist/style.css";
 
-const xNums = [0.351, 0.867, 3.315, 5.013, 6.432];
-const yNums = [-0.572, -2.015, -3.342, -5.752, -6.911];
+const xNums = [0.015, 0.681, 1.342, 2.118, 2.671];
+const yNums = [-2.417, -3.819, -0.642, 0.848, 2.815];
 
 function computeLagrangeFn() {
-  const polynoms =
-    createBasisPolynoms(xNums);
+  const polynoms = createBasisPolynoms(xNums);
 
-  const iterpolatedLagrangePolynom =
-    interpolateByLagrange(polynoms, yNums);
+  const iterpolatedLagrangePolynom = interpolateByLagrange(polynoms, yNums);
 
   return toFn(iterpolatedLagrangePolynom);
 }
@@ -21,15 +20,13 @@ function computeLagrangeFn() {
 class App extends Component {
   lagrangeFn = computeLagrangeFn();
 
-
   render() {
-    const renderData = xNums
-      .map((it, index) => ({x: it, y: yNums[index]}));
+    const renderData = xNums.map((it, index) => ({ x: it, y: yNums[index] }));
     return (
       <React.Fragment>
         <div className="charts-container">
-          <Chart data={renderData}/>
-          <Chart fn={this.lagrangeFn}/>
+          <Chart data={renderData} />
+          <Chart fn={this.lagrangeFn} maxX={max(xNums)} />
         </div>
       </React.Fragment>
     );
--- src/Chart.js
+++ src/Chart.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { HorizontalGridLines, LineSeries, XAxis, XYPlot, YAxis } from 'react-vis';
-import range from 'lodash/range';
-import 'react-vis/dist/style.css'
+import React from "react";
+import {
+  HorizontalGridLines,
+  LineSeries,
+  XAxis,
+  XYPlot,
+  YAxis
+} from "react-vis";
+import range from "lodash/range";
+import "react-vis/dist/style.css";
 
-function generateDataSet(fn, step = 0.1, max = 6) {
-  return range(0, max, step)
-    .map(x => ({x: x, y: fn(x)}));
+function generateDataSet(fn, max = 6, step = 0.1) {
+  return range(0, max, step).map(x => ({ x: x, y: fn(x) }));
 }
 
-export default ({fn, data}) => {
-  const dataSet = data ? data : generateDataSet(fn);
+export default ({ fn, data, maxX }) => {
+  const dataSet = data ? data : generateDataSet(fn, maxX);
 
   return (
-    <XYPlot
-      width={300}
-      height={300}>
-      <HorizontalGridLines/>
-      <LineSeries
-        data={dataSet}/>
-      <XAxis/>
-      <YAxis/>
+    <XYPlot width={300} height={300}>
+      <HorizontalGridLines />
+      <LineSeries data={dataSet} />
+      <XAxis />
+      <YAxis />
     </XYPlot>
   );
 };
